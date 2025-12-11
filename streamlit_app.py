814bbb55b3015738443f6fab333890c7fc1ef6bc
Marisolxiu <marisolxiuhan@gmail.com>
2025-12-11 20:02:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Minha Biblioteca Pessoal")
 st.header("Começa agora", divider=True)
 st.subheader("Apaixoandos pela literatura")
-st.write("Seja Bem-Vindo ao nosso site onde voce tera a sua biblioteca totalmente pessoal")
+st.write("Sejam Bem-Vindos ao nosso site onde voce tera a sua biblioteca totalmente pessoal")
